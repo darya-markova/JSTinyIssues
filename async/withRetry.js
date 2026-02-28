7cb44cbfb6c13ba6c190927732e4af82132d650d
darya-markova <darya.towarddreams@gmail.com>
2026-02-28 11:32:28 +0300

    Добавлен прототип простейшей функции retry

--- async/withRetry.js
+++ async/withRetry.js
@@ -1,1 +1,19 @@
-// Использовать и написать функцию withRetry, как правильно это сделать?
+function withRetry(fn, limit) {
+  return async function (...args) {
+    const errors = [];
+
+    for (let attempt = 1; attempt <= limit; attempt++) {
+      try {
+        const result = await fn(...args);
+        return result;
+      } catch (error) {
+        errors.push(error);
+
+        if (attempt === limit) {
+          throw new AggregateError(errors, 'Too Many Calls')
+        }
+        continue;
+      }
+    }
+  }
+}
