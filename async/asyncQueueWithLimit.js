b63a92dbe9f68a1ad8299ebd599b5a088885a350
darya-markova <darya.towarddreams@gmail.com>
2026-03-09 15:25:38 +0300

    async queue with limit realization

--- async/asyncQueueWithLimit.js
+++ async/asyncQueueWithLimit.js
@@ -1,14 +1,43 @@
 class AsyncQueueWithLimit {
-  constructor(concurrencyLimit) {}
-
-  enqueue(task) {
+  constructor(concurrencyLimit) {
+    this.concurrencyLimit = concurrencyLimit;
+    
+    this.queue = [];
+    this.runningCount = 0;
   }
-}
-// enqueue
-// enqueue
-// enqueue
-/*
-  Если в очереди есть место, то добавленная с помощью метода enqueue задача должна запуститься сразу. 
-  Если нет, то она добавляется в очередь и выполняется, когда место осовободится. 
-  Функции запускаются в порядке добавления в очередь.
-*/
+
+  async enqueue(task) {
+    return new Promise((resolve, reject) => {
+      const queueItem = { task, resolve, reject };
+
+      if (this.runningCount < this.concurrencyLimit) {
+        this.runTask(queueItem);
+      } else {
+        this.queue.push(queueItem);
+      }
+    });
+  }
+
+  async runTask() {
+    const { task, resolve, reject } = queueItem;
+
+    this.runningCount++;
+
+    try {
+      const result = await task();
+      resolve(result);
+    } catch (error) {
+      reject(error);
+    } finally {
+      this.runningCount--;
+      this.processNextTask();
+    }
+  }
+
+   processNextTask() {
+    if (this.queue.length > 0 && this.runningCount < this.concurrencyLimit) {
+      const nextTask = this.queue.shift();
+      this.runTask(nextTask);
+    }
+  }
+} 
